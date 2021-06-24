6786702a2aacd90a9089886a9d4eca5aab1d6e70
manai bilel <bilel_kata@live.fr>
2021-06-24 21:15:49 +0200

    chckexpress

---
+++ app.js
@@ -0,0 +1,51 @@
+const { render } = require("ejs");
+const express = require("express");
+const app = express();
+app.listen(3100);
+app.set("view engine", "ejs");
+app.use(express.static("./views/style"));
+app.use((req, res, next) => {
+  let date = new Date();
+  if (
+    date.getDay() == 0 ||
+    date.getDay() == 6 ||
+    date.getHours() < 9 ||
+    date.getHours() >= 17
+  ) {
+    res.render("404", { title: "we are closed", date: new Date() });
+  }
+  next();
+});
+app.get("/", (req, res) => {
+  const blogs = [
+    {
+      title: "Where does it come from?",
+      snippet:
+        "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of 'de Finibus Bonorum et Malorum' (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, 'Lorem ipsum dolor sit amet..', comes from a line in section 1.10.3  The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from de Finibus Bonorum et Malorum by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.,",
+    },
+    {
+      title: "Why do we use it?",
+      snippet:
+        "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
+    },
+    {
+      title: "Where can I get some?",
+      snippet:
+        "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.",
+    },
+  ];
+  res.render("home", { title: "Home", blogs });
+});
+
+app.get("/ourServices", (req, res) => {
+  res.render("ourServices", { title: "ourServices" });
+});
+
+app.get("/contactus", (req, res) => {
+  res.render("contactus", { title: "contactus" });
+});
+
+
+app.use((req, res) => {
+  res.status(404).render("404", { title: "404", date: new Date() });
+});
